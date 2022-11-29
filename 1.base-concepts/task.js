f6bbe5dcee906209adc7567ca3dd58c1497fbfe5
pOlegV <oleg-1@yandex.ru>
2022-11-29 14:44:48 +0300

    ДЗ 2 внесение правок

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,9 +2,7 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let d = Math.pow(b, 2) - 4 * a * c;
-  if (d < 0){
-    return (arr = []);
-  }else if (d === 0){
+  if (d === 0){
     arr.push(-b/(2*a)); 
   }else if (d > 0){
     arr.push((-b + Math.sqrt(d)) / (2 * a));
@@ -14,12 +12,13 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  if (!isNaN(percent) && !isNaN(contribution) && !isNaN(amount) && !isNaN(countMonths)){
-    let s = amount - contribution;
-    let p = percent / 100 / 12;
-    let pay = s * ( p + ( p / ((Math.pow(( 1 + p ), countMonths )) - 1 )));
-    let allPay = pay * countMonths;
-    return parseFloat(allPay.toFixed(2));
+  if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)){
+    return false;
   }
-  return false;
+  
+  let s = amount - contribution;
+  let p = percent / 100 / 12;
+  let pay = s * ( p + ( p / ((Math.pow(( 1 + p ), countMonths )) - 1 )));
+  let allPay = pay * countMonths;
+  return parseFloat(allPay.toFixed(2));
 }
