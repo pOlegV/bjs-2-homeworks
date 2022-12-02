1d40cc1d1b1ffa439f93cc72b66954569b8d2ad6
pOlegV <oleg-1@yandex.ru>
2022-12-02 15:28:16 +0300

    ДЗ 4 готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.length === arr2.length && arr1.every((number, item) => number === arr2[item]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    if (users instanceof Array && users.length > 0 && (gender === "мужской" || gender === "женский")){
+        let allAge = users.filter(user => user.gender === gender).map(ages => ages.age);
+        let average = allAge.reduce((age, item) => age + item) / allAge.length;
+       return  average;
+    }else {
+        return 0;
+    }
+    
 }
