5f9f99565d16e6607cf7afd64c89d8255bfe0eea
pOlegV <oleg-1@yandex.ru>
2022-12-02 11:26:57 +0300

    ДЗ 2 и 3 перенос в другую ветку

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,24 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = Math.pow(b, 2) - 4 * a * c;
+  if (d === 0){
+    arr.push(-b/(2*a)); 
+  }else if (d > 0){
+    arr.push((-b + Math.sqrt(d)) / (2 * a));
+    arr.push((-b - Math.sqrt(d)) / (2 * a));
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)){
+    return false;
+  }
   
+  let s = amount - contribution;
+  let p = percent / 100 / 12;
+  let pay = s * ( p + ( p / ((Math.pow(( 1 + p ), countMonths )) - 1 )));
+  let allPay = pay * countMonths;
+  return parseFloat(allPay.toFixed(2));
 }
--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,79 @@
 function getArrayParams(...arr) {
-
-  return { min: min, max: max, avg: avg };
-}
+  if (!isNaN(arr[0])){
+    let min = arr[0];
+    let max = arr[0];
+    let sum = 0;
+    let avg = 0;
+    for (let i = 0; i < arr.length; i++){
+      if (min > arr[i]){min = arr[i]};
+      if (max < arr[i]) {max = arr[i]};
+      sum += arr[i];
+    }
+    avg = Number((sum / arr.length).toFixed(2));
+    return { min: min, max: max, avg: avg };
+  }
+  else{return 0;}
+} 
 
 function summElementsWorker(...arr) {
-
+  if (!isNaN(arr[0])){
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++){
+      sum += arr[i];
+    }
+    return sum;
+  }
+  else{return 0;}
 }
 
 function differenceMaxMinWorker(...arr) {
-
+  if (!isNaN(arr[0])){
+    let differenceMaxMin = getArrayParams(...arr);
+    let max = differenceMaxMin.max;
+    let min = differenceMaxMin.min;
+    return max - min;
+  }
+  else{return 0;}
+ 
 }
 
 function differenceEvenOddWorker(...arr) {
-
+  if (!isNaN(arr[0])){
+    let sumEvenElement = 0;
+    let sumOddElement = 0;
+    for (let i = 0; i < arr.length; i++){
+      if (arr[i] % 2 === 0){
+        sumEvenElement += arr[i];
+      }
+      else{
+        sumOddElement += arr[i];
+      }
+    }
+    return sumEvenElement - sumOddElement;
+  }
+  else{return 0;}
 }
 
 function averageEvenElementsWorker(...arr) {
-
+  if (!isNaN(arr[0])){
+    let sumEvenElement = 0;
+    let countEvenElement = 0;
+    for (let i = 0; i < arr.length; i++){
+      if (arr[i] % 2 === 0){
+        sumEvenElement += arr[i];
+        countEvenElement ++;
+      }
+    }
+    return sumEvenElement / countEvenElement;
+  }
+  else{return 0;}
 }
 
 function makeWork (arrOfArr, func) {
-
+  let maxWorkerResult = -Infinity;
+  for (let i = 0; i < arrOfArr.length; i++){
+    let maxFunc = func(...arrOfArr[i]);
+    if (maxFunc > maxWorkerResult) {maxWorkerResult = maxFunc;}
+  }
+  return maxWorkerResult;
 }
