dd65f95101d479773a7106df043828cf7d636bd0
pOlegV <oleg-1@yandex.ru>
2022-12-15 11:32:55 +0300

    ДЗ 5 готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,30 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (this.excluded === undefined) {
+        this.marks.push(...marks);
+      }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (this.marks === undefined || this.marks.length === 0) {
+        return 0;
+      } else {
+          return this.marks.reduce((accum, current) => accum + current, 0) / this.marks.length;
+      }    
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
